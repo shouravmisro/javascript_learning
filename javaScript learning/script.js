c92c55cc6e54878e8e00545653c28fb87a59558b
shouravmisro <mshourav05@gmail.com>
2025-06-03 00:55:41 +0600

    day 1 , simple input and output display

--- javaScript learning/script.js
+++ javaScript learning/script.js
@@ -1,1 +1,8 @@
-console.log("Hello, World!");
+const btnelement = document.querySelector("button");
+const spanElement = document.getElementById
+("updateName");
+
+btnelement.onclick = function () {
+    const yourname = prompt("Enter your name");
+    spanElement.textContent = yourname;
+}
