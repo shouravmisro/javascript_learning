5a4c1670267c603f0afcc7c783a38145cfde5801
shouravmisro <mshourav05@gmail.com>
2025-06-06 03:51:56 +0600

    random password

--- script3.js
+++ script3.js
@@ -7,6 +7,34 @@ genrateRandomNumber.addEventListener("click", () => {
 
 const resetButton = document.getElementById("reset-button");
 resetButton.addEventListener("click", () => {
-  randomNumber.textContent = "0";
+  randomNumber.textContent = "From 1 to 1000";
   genrateRandomNumber.disabled = false;
+});
+
+const passbox = document.getElementById("password");
+const rmp = document.getElementById("rmp");
+
+const genaratepassword = () => {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+#$%^&";
+  let password = "";
+  for (let i = 0; i < 12; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    password += characters[randomIndex];
+  }
+
+  passbox.value = password;
+};
+rmp.addEventListener("click", genaratepassword);
+
+const copyButton = document.getElementById("copy");
+copyButton.addEventListener("click", () => {
+  passbox.select();
+  document.execCommand("copy");
+  alert("Password copied to clipboard!");
+});
+
+const resetButton2 = document.getElementById("reset-button2");
+resetButton2.addEventListener("click", () => {
+  passbox.value = "";
 });
